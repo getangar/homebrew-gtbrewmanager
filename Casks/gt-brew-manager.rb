68256987ad9cba9ae38f6fdaa7cc16c8d39f1060
Gennaro Eduardo Tangari <gennarotangari@icloud.com>
2026-07-27 11:09:59 +0200

    Fixing url

--- Casks/gt-brew-manager.rb
+++ Casks/gt-brew-manager.rb
@@ -2,7 +2,7 @@ cask "gt-brew-manager" do
   version "1.0-43"
   sha256 "d258ecf5ffbf3a12217ad5f4567732610c329dde0bc10b6c61961149bd5e38d9"
 
-  url "https://github.com/getangar/GTBrewManager/releases/download/v#{version}/GT.Brew.Manager.dmg"
+  url "https://github.com/getangar/homebrew-gtbrewmanager/releases/download/v1.0-43/GT.Brew.Manager.dmg"
   name "GT Brew Manager"
   desc "Native macOS GUI for Homebrew formulae, casks and services"
   homepage "https://github.com/getangar/GTBrewManager"
