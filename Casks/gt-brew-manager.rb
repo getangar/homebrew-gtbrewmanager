2b732da70bcda8b9a9eda44069fffd64858d63b8
Gennaro Eduardo Tangari <gennarotangari@icloud.com>
2026-07-27 11:04:06 +0200

    Add gt-brew-manager cask v1.0-43

---
+++ Casks/gt-brew-manager.rb
@@ -0,0 +1,16 @@
+cask "gt-brew-manager" do
+  version "1.0-43"
+  sha256 "d258ecf5ffbf3a12217ad5f4567732610c329dde0bc10b6c61961149bd5e38d9"
+
+  url "https://github.com/getangar/GTBrewManager/releases/download/v#{version}/GT.Brew.Manager.dmg"
+  name "GT Brew Manager"
+  desc "Native macOS GUI for Homebrew formulae, casks and services"
+  homepage "https://github.com/getangar/GTBrewManager"
+
+  app "GT Brew Manager.app"
+
+  zap trash: [
+    "~/Library/Application Support/GTBrewManager",
+    "~/Library/Preferences/com.gennarotangari.GTBrewManager.plist",
+  ]
+end
