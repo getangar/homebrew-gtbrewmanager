383bc891f42ea506e17e046cc1cead96cbe08d57
Gennaro Eduardo Tangari <gennarotangari@icloud.com>
2026-07-27 14:00:29 +0200

    chore: bump version to 1.0.1-50

--- Casks/gt-brew-manager.rb
+++ Casks/gt-brew-manager.rb
@@ -1,5 +1,5 @@
 cask "gt-brew-manager" do
-  version "1.0-43"
+  version "1.0.1-50"
   sha256 "43d1cd92e202a5a5776e96aef8fc9abf951f0de87935510fc3187f044216f171"
 
   url "https://github.com/getangar/homebrew-gtbrewmanager/releases/download/v1.0.1-50/GT.Brew.Manager.dmg"
