b3be3570944ad4699c272211e46515d8c3621e3f
Gennaro Eduardo Tangari <gennarotangari@icloud.com>
2026-07-27 14:48:08 +0200

    Update gt-brew-manager version to 1.0.5-55

--- Casks/gt-brew-manager.rb
+++ Casks/gt-brew-manager.rb
@@ -1,8 +1,8 @@
 cask "gt-brew-manager" do
-  version "1.0.1-50"
+  version "1.0.5-55"
   sha256 "43d1cd92e202a5a5776e96aef8fc9abf951f0de87935510fc3187f044216f171"
 
-  url "https://github.com/getangar/homebrew-gtbrewmanager/releases/download/v1.0.1-50/GT.Brew.Manager.dmg"
+  url "https://github.com/getangar/homebrew-gtbrewmanager/releases/download/v1.0.5-55/GT.Brew.Manager.dmg"
   name "GT Brew Manager"
   desc "Native macOS GUI for Homebrew formulae, casks and services"
   homepage "https://github.com/getangar/GTBrewManager"
